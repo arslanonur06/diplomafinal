cddf1e16d1fd12838f4381bb30a75ece3609509f
arslanonur06 <120748816+arslanonur06@users.noreply.github.com>
2025-04-28 23:09:30 +0500

    N

--- 28mart/diplom/social-network-platform/frontend/vite.config.ts
+++ 28mart/diplom/social-network-platform/frontend/vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@contexts': path.resolve(__dirname, './src/contexts')
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+    },
   },
   build: {
+    outDir: "dist",
     sourcemap: true,
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
+    },
   },
   server: {
-    port: 3004
-  }
+    port: 3004,
+  },
+  base: "/",
 });
