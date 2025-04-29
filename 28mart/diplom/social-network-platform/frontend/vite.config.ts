d927789c06407a982c1c35f3ced79eee67e345d2
arslanonur06 <120748816+arslanonur06@users.noreply.github.com>
2025-04-29 16:48:13 +0500

    new

--- 28mart/diplom/social-network-platform/frontend/vite.config.ts
+++ 28mart/diplom/social-network-platform/frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
